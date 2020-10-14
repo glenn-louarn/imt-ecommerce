1fb2b1843f9ee846062b03c83fcb95329d6f2ba5
glenn_louarn <glenn.louarn@gmail.com>
2020-10-14 14:44:07 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BeerComponent } from './beer/beer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        MenuComponent,
+        FooterComponent,
+        BeerComponent,
     ],
     imports: [
         BrowserModule,
